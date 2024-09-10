7dd97f0f7f0cf5da844d60e3795c0aac7755dbe6
Xanssun <115156090+Xanssun@users.noreply.github.com>
2024-09-10 20:31:17 +0500

    hash func

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"crypto/sha256"
+	"fmt"
+	"net/http"
+)
+
+var urlHash = make(map[string]string)
+
+func HashUrl(url string) string {
+	hash := sha256.New()
+	hash.Write([]byte(url))
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+func main() {
+	http.ListenAndServe(":8080", nil)
+
+}
